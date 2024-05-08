c616141f054d0b676b725256b870de46fe9b3ec8
efdwguhasijo <gs1powers3x@hotmail.com>
2024-05-08 23:49:30 +0700

    start

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
-function maximalRectangle(matrix) {
-  if (matrix.length === 0 || matrix[0].length === 0) return 0;
-  const rows = matrix.length;
-  const cols = matrix[0].length;
-  const heights = new Array(cols).fill(0);
-  let maxArea = 0;
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      heights[j] = matrix[i][j] === "1" ? heights[j] + 1 : 0;
-    }
-    maxArea = Math.max(maxArea, largestRectangleArea(heights));
+function longestPalindrome(s) {
+  const map = new Map();
+  let hasOdd = false;
+  let result = 0;
+  for (const char of s) {
+    map.set(char, (map.get(char) || 0) + 1);
   }
-  return maxArea;
-  function largestRectangleArea(heights) {
-    const stack = [];
-    let maxArea = 0;
-    for (let i = 0; i <= heights.length; i++) {
-      while (
-        stack.length &&
-        (i === heights.length || heights[i] < heights[stack[stack.length - 1]])
-      ) {
-        const height = heights[stack.pop()];
-        const width = stack.length === 0 ? i : i - stack[stack.length - 1] - 1;
-        maxArea = Math.max(maxArea, height * width);
-      }
-      stack.push(i);
+  for (const count of map.values()) {
+    if (count % 2 === 0) result += count;
+    else {
+      result += count - 1;
+      hasOdd = true;
     }
-    return maxArea;
   }
+  return hasOdd ? result + 1 : result;
 }
