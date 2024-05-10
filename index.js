fdf552ecdf04de71091b50cc77a50fdd9af567b7
efdwguhasijo <gs1powers3x@hotmail.com>
2024-05-10 12:47:57 +0700

    update task

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function rotateArray(arr, n) {
-  return arr.slice(n).concat(arr.slice(0, n));
+function minMeetingRooms(intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+  const minHeap = new MinHeap();
+  for (const interval of intervals) {
+    if (minHeap.size() > 0 && minHeap.peek() <= interval[0]) {
+      minHeap.pop();
+    }
+    minHeap.push(interval[1]);
+  }
+  return minHeap.size();
 }
