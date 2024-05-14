1ea68895a7a88c3de9fa81728188989ff18281c6
efdwguhasijo <gs1powers3x@hotmail.com>
2024-05-14 23:08:33 +0700

    update commit

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-function wiggleMaxLength(nums) {
-  if (nums.length === 0) return 0;
-  let up = 1;
-  let down = 1;
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] > nums[i - 1]) {
-      up = down + 1;
-    } else if (nums[i] < nums[i - 1]) {
-      down = up + 1;
-    }
-  }
-  return Math.max(up, down);
+function maxDepth(root) {
+  if (!root) return 0;
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 }
